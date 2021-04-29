78bf3fc6f30c3b3d6378d67a116d5a1e0bc66f03
kaiqkt <kaique..gomes@bandtec.com.br>
2021-04-29 20:52:56 -0300

    second

--- src/main/kotlin/com/gotravel/GoTravelServiceApplication.kt
+++ src/main/kotlin/com/gotravel/GoTravelServiceApplication.kt
@@ -7,5 +7,6 @@ import org.springframework.boot.runApplication
 class GoTravelServiceApplication
 
 fun main(args: Array<String>) {
+	print("hello")
 	runApplication<GoTravelServiceApplication>(*args)
 }
